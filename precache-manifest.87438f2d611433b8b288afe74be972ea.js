bc734a7207177bf3278b59c324dd616027654eeb
ozair <truemail785@gmail.com>
2020-09-04 16:06:35 +0500

    Updates

---
+++ precache-manifest.87438f2d611433b8b288afe74be972ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0fe791c4f1b701e66b10ed8015a0e7",
+    "url": "/random-quote/index.html"
+  },
+  {
+    "revision": "a5a35f39cfca5254ff94",
+    "url": "/random-quote/static/css/main.df242713.chunk.css"
+  },
+  {
+    "revision": "771e114fe7245d282675",
+    "url": "/random-quote/static/js/2.f7d2a031.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/random-quote/static/js/2.f7d2a031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a35f39cfca5254ff94",
+    "url": "/random-quote/static/js/main.5da775c0.chunk.js"
+  },
+  {
+    "revision": "5d574528464f68a2bba7",
+    "url": "/random-quote/static/js/runtime-main.65db5d59.js"
+  }
+]);
